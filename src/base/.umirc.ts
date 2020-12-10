3e4a8308d66efc24c76151e733d83bc5b34c06bf
王超玉 <chaoyumail@126.com>
2020-12-10 18:26:59 +0800

    :triangular_flag_on_post: 优化框架，基座使用react，基于umi

--- src/base/.umirc.ts
+++ src/base/.umirc.ts
@@ -1,40 +1,10 @@
-import { IConfig } from 'umi-types';
+import { defineConfig } from 'umi';
 
-// ref: https://umijs.org/config/
-const config: IConfig =  {
-  treeShaking: true,
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
   routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+    { path: '/', component: '@/pages/index' },
   ],
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'base',
-      dll: true,
-      locale: {
-        enable: true,
-        default: 'en-US',
-      },
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
-}
-
-export default config;
+});
