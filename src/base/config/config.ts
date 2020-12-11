baa07026385308426d1c1ec2e4ad279aecc8d280
王超玉 <chaoyumail@126.com>
2020-12-11 14:13:23 +0800

    :package: 基座的基础配置

---
+++ src/base/config/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'umi';
+import routes from './routes'
+import defaultSettings from './defaultSettings'
+import proxy from './proxy'
+
+const { REACT_APP_ENV } = process.env;
+console.log('REACT_APP_ENV: ', REACT_APP_ENV);
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  hash: true,
+  antd: {},
+  dva: {
+    hmr: true,
+  },
+  layout: {
+    name: 'micro-frontend',
+    locale: true,
+    siderWidth: 208,
+    ...defaultSettings,
+  },
+  dynamicImport: {
+    loading: '@ant-design/pro-layout/es/PageLoading',
+  },
+  targets: {
+    ie: 11,
+  },
+  routes,
+  theme: {
+    'primary-color': defaultSettings.primaryColor,
+  },
+  title: false,
+  ignoreMomentLocale: true,
+  // proxy: proxy[REACT_APP_ENV || 'dev'],
+  proxy: proxy['dev'],
+  manifest: {
+    basePath: '/',
+  },
+  history: {
+    type: 'browser',
+  },
+  exportStatic: {},
+});
